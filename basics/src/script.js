bee7854831501d1680027d7942f900d1c07c4358
Cheyoon Jung <jessietwo94@gmail.com>
2022-11-03 10:40:10 +0100

    update

--- basics/src/script.js
+++ basics/src/script.js
@@ -1,59 +1,51 @@
-import './style.css'
-import * as THREE from 'three'
+import "./style.css";
+import * as THREE from "three";
+import gsap from "gsap";
 
+console.log(gsap);
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
-
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-//time
-let time = Date.now()
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+gsap.to(mesh.rotation, { duration: 1, delay: 1, y: 2 });
 
 //animation
 const tick = () => {
-    //time
-    const currentTime = Date.now()
-    const deltaTime = currentTime - time
-    time= currentTime
-    //update obj
-    // mesh.position.x += 0.01
-    mesh.rotation.y += 0.01
-
-    //render
-    renderer.render(scene,camera)
-    window.requestAnimationFrame(tick)
-}
-tick()
+  //render
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+tick();
