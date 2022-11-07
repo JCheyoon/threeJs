b4aec242e4566cf713519975045e3afe71b00b4d
Cheyoon Jung <jessietwo94@gmail.com>
2022-11-07 14:17:09 +0100

    learning cameras

--- basics/src/script.js
+++ basics/src/script.js
@@ -1,36 +1,62 @@
 import "./style.css";
 import * as THREE from "three";
-import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-console.log(gsap);
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
-
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Objects
- */
+/**Objects*/
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-/**
- * Sizes
- */
+/** Sizes*/
 const sizes = {
   width: 800,
   height: 600,
 };
 
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+/** cursor */
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (e) => {
+  //-0.5 하는이유는 오른쪽 -를 +값으로 바꾸기위해, y값은 three.js에서 아래가 +값이여서 reverse 해준다
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = -(e.clientY / sizes.height - 0.5);
+});
+
+/**Camera*/
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+
+/*
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio,
+  1 * aspectRatio,
+  1,
+  -1,
+  0.1,
+  100
+);*/
+
+camera.position.z = 2;
+camera.lookAt(mesh.position);
 scene.add(camera);
+
+//controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
@@ -40,10 +66,21 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 
-gsap.to(mesh.rotation, { duration: 1, delay: 1, y: 2 });
+const clock = new THREE.Clock();
 
 //animation
 const tick = () => {
+  //Clock
+  // const elapsedTime = clock.getElapsedTime();
+
+  // update cam
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 10;
+  // camera.lookAt(mesh.position);
+
+  //update controls each frame
+  controls.update();
   //render
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
