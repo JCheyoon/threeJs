9fc265e5577f8b3b7058380e7b1c5d9127c46274
Cheyoon Jung <jessietwo94@gmail.com>
2022-11-15 16:37:49 +0100

    learning text

--- basics/src/script.js
+++ basics/src/script.js
@@ -2,140 +2,73 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 
-/**
- * Texture
- */
-const textureLoader = new THREE.TextureLoader();
-
-const doorTexture = textureLoader.load("/textures/door/basecolor.jpg");
-const doorAlphaTexture = textureLoader.load("/textures/door/alpha.jpg");
-const doorAmbientOcclusionTexture = textureLoader.load(
-  "/textures/door/ambientOcclusion.jpg"
-);
-const doorHeightTexture = textureLoader.load("/textures/door/height.png");
-const doorNormalTexture = textureLoader.load("/textures/door/normal.jpg");
-const doorMetalnessTexture = textureLoader.load("/textures/door/metallic.jpg");
-const doorRoughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
-const matcapTexture = textureLoader.load("/textures/matcaps/3.png");
-const gradientTexture = textureLoader.load("/textures/gradients/3.jpg");
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-
-const environmentMapTexture = cubeTextureLoader.load([
-  "/textures/environmentMaps/0/px.jpg",
-  "/textures/environmentMaps/0/nx.jpg",
-  "/textures/environmentMaps/0/py.jpg",
-  "/textures/environmentMaps/0/ny.jpg",
-  "/textures/environmentMaps/0/pz.jpg",
-  "/textures/environmentMaps/0/nz.jpg",
-]);
 /**
  * Base
  */
+// Debug
+const gui = new dat.GUI();
+
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
 
+//axes helper
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
 /**
- * object
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load("/textures/matcaps/8.png");
+/**
+ *
+ * Font
  */
 
-// const material = new THREE.MeshBasicMaterial();
-// material.map = doorTexture;
-// material.transparent = true;
-// material.alphaMap = doorAlphaTexture;
-// material.side = THREE.DoubleSide;
+const fontLoader = new FontLoader();
 
-// const material = new THREE.MeshNormalMaterial();
-// material.flatShading = true;
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+  // Material
+  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
 
-// const material = new THREE.MeshMatcapMaterial();
-// material.matcap = matcapTexture;
+  // Text
+  const textGeometry = new TextGeometry("Petike", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  textGeometry.center();
 
-// const material = new THREE.MeshLambertMaterial();
+  const text = new THREE.Mesh(textGeometry, material);
+  scene.add(text);
 
-// const material = new THREE.MeshPhongMaterial();
-// material.shininess = 100;
-// material.specular = new THREE.Color(0x1188ff);
+  // Donuts
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64);
 
-// const material = new THREE.MeshToonMaterial();
-// material.gradientMap = gradientTexture;
-//
-// gradientTexture.minFilter = THREE.NearestFilter;
-// gradientTexture.magFilter = THREE.NearestFilter;
-// gradientTexture.generateMipmaps = false;
+  for (let i = 0; i < 100; i++) {
+    const donut = new THREE.Mesh(donutGeometry, material);
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+    const scale = Math.random();
+    donut.scale.set(scale, scale, scale);
 
-// const material = new THREE.MeshStandardMaterial();
-// material.metalness = 0.45;
-// material.roughness = 0.65;
-// material.map = doorTexture;
-// material.aoMap = doorAmbientOcclusionTexture;
-// material.aoMapIntensity = 1;
-// material.displacementMap = doorHeightTexture;
-// material.displacementScale = 0.1;
-// material.metalnessMap = doorMetalnessTexture;
-// material.roughnessMap = doorRoughnessTexture;
-// material.normalMap = doorNormalTexture;
-// material.normalScale.set(0.3, 0.3);
-// material.transparent = true;
-// material.alphaMap = doorAlphaTexture;
-
-//environment
-const material = new THREE.MeshStandardMaterial();
-material.metalness = 0.7;
-material.roughness = 0.2;
-
-material.envMap = environmentMapTexture;
-/**
- * Debug
- */
-const gui = new dat.GUI();
-gui.add(material, "metalness").min(0).max(1).step(0.0001);
-gui.add(material, "roughness").min(0).max(1).step(0.0001);
-
-const sphere = new THREE.Mesh(
-  new THREE.SphereBufferGeometry(0.5, 16, 16),
-  material
-);
-sphere.position.x = -1.5;
-
-sphere.geometry.setAttribute(
-  "uv2",
-  new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
-);
-const plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(1, 1), material);
-
-plane.geometry.setAttribute(
-  "uv2",
-  new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
-);
-const torus = new THREE.Mesh(
-  new THREE.TorusBufferGeometry(0.3, 0.2, 16, 32),
-  material
-);
-torus.position.x = 1.5;
-
-torus.geometry.setAttribute(
-  "uv2",
-  new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2)
-);
-scene.add(sphere, plane, torus);
-
-/**
- * Lights
- */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
-
-// ...
-
-const pointLight = new THREE.PointLight(0xffffff, 0.5);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
+    scene.add(donut);
+  }
+});
 
 /**
  * Sizes
@@ -194,11 +127,6 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
-  //update obj
-  sphere.rotation.y = 0.1 * elapsedTime;
-  plane.rotation.y = 0.1 * elapsedTime;
-  torus.rotation.y = 0.1 * elapsedTime;
 
   // Update controls
   controls.update();
