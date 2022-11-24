23d31a540721e841b0ae9a96977475512de4ef42
Cheyoon Jung <jessietwo94@gmail.com>
2022-11-24 16:59:20 +0100

    learning galxy

--- classicTechniques/src/script.js
+++ classicTechniques/src/script.js
@@ -7,7 +7,7 @@ import * as dat from "lil-gui";
  * Base
  */
 // Debug
-const gui = new dat.GUI();
+const gui = new dat.GUI({ width: 360 });
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -15,60 +15,92 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader();
-const particleTexture = textureLoader.load("/textures/particles/2.png");
+const parameters = {};
+parameters.count = 1000;
+parameters.size = 0.02;
+parameters.radius = 5;
+parameters.branches = 3;
 
-/**
- * particle
- */
-// //geo
-// const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
-// // Material
-// const particlesMaterial = new THREE.PointsMaterial({
-//   size: 0.02,
-//   sizeAttenuation: true,
-// });
-// // Points
-// const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-// scene.add(particles);
+let geometry = null;
+let material = null;
+let points = null;
 
-const particlesGeometry = new THREE.BufferGeometry();
-const count = 10000;
+const generateGalaxy = () => {
+  if (points !== null) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(points);
+  }
+  /**Geometry*/
+  geometry = new THREE.BufferGeometry();
 
-const positions = new Float32Array(count * 3); // Multiply by 3 because each position is composed of 3 values (x, y, z)
-const colors = new Float32Array(count * 3);
+  const positions = new Float32Array(parameters.count * 3); //xyz
+  const radius = Math.random() * parameters.radius;
+  //random value
 
-for (let i = 0; i < count * 3; i++) {
-  positions[i] = (Math.random() - 0.5) * 10;
-  colors[i] = Math.random();
-}
+  for (let i = 0; i < parameters.count; i++) {
+    const i3 = i * 3;
+    const radius = Math.random() * parameters.radius;
+    const brancheAngle = (i % parameters.branches) / parameters.branches;
+    for (let i = 0; i < parameters.count; i++) {
+      const i3 = i * 3;
 
-particlesGeometry.setAttribute(
-  "position",
-  new THREE.BufferAttribute(positions, 3)
-);
-particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+      const radius = Math.random() * parameters.radius;
+      const branchAngle =
+        ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
 
-// Material
-const particlesMaterial = new THREE.PointsMaterial({
-  color: "#5C43F5",
-  map: particleTexture,
-  size: 0.1,
-  sizeAttenuation: true,
-});
-particlesMaterial.transparent = true;
-particlesMaterial.alphaMap = particleTexture;
-// particlesMaterial.alphaTest = 0.001
-// particlesMaterial.depthTest = false;
-particlesMaterial.depthWrite = false;
-particlesMaterial.blending = THREE.AdditiveBlending;
-particlesMaterial.vertexColors = true;
-// Points
-const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(particles);
+      positions[i3] = Math.cos(branchAngle) * radius;
+      positions[i3 + 1] = 0;
+      positions[i3 + 2] = Math.sin(branchAngle) * radius;
+    }
+
+    positions[i3] = radius;
+    positions[i3 + 1] = 0;
+    positions[i3 + 2] = 0;
+  }
+
+  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+
+  /**Material*/
+  material = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+  });
+  /**Points*/
+  points = new THREE.Points(geometry, material);
+  scene.add(points);
+};
+generateGalaxy();
+
+gui
+  .add(parameters, "count")
+  .min(100)
+  .max(10000)
+  .step(100)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "size")
+  .min(0.001)
+  .max(0.1)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+
+gui
+  .add(parameters, "radius")
+  .min(0.01)
+  .max(20)
+  .step(0.01)
+  .onFinishChange(generateGalaxy);
+
+gui
+  .add(parameters, "branches")
+  .min(2)
+  .max(20)
+  .step(1)
+  .onFinishChange(generateGalaxy);
+
 /**
  * Sizes
  */
@@ -101,6 +133,8 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
+camera.position.x = 3;
+camera.position.y = 3;
 camera.position.z = 3;
 scene.add(camera);
 
@@ -124,15 +158,6 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-  for (let i = 0; i < count; i++) {
-    let i3 = i * 3;
-
-    const x = particlesGeometry.attributes.position.array[i3];
-    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(
-      elapsedTime + x
-    );
-  }
-  particlesGeometry.attributes.position.needsUpdate = true;
 
   // Update controls
   controls.update();
