713cec95a543d0dc5a7fbdb82b9e92271a14ecd2
Cheyoon Jung <jessietwo94@gmail.com>
2022-12-27 16:04:34 +0100

    learning

--- advancedTechniques/src/Experience/Experience.js
+++ advancedTechniques/src/Experience/Experience.js
@@ -1,9 +1,16 @@
 import * as THREE from "three";
 import Sizes from "./Utils/Sizes";
 import Time from "./Utils/Time";
+import Camera from "./Camera";
+
+let instance = null;
 
 export default class Experience {
   constructor(canvas) {
+    if (instance) {
+      return instance;
+    }
+    instance = this;
     //global acess
     window.experience = this;
 
@@ -14,6 +21,7 @@ export default class Experience {
     this.sizes = new Sizes();
     this.time = new Time();
     this.scene = new THREE.Scene();
+    this.camera = new Camera();
 
     //size resize event
     this.sizes.on("resize", () => {
