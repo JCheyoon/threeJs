c290241fcede39555bebef1dffe73f710bb044f1
Cheyoon Jung <jessietwo94@gmail.com>
2023-03-09 13:16:26 +0100

    varying

--- Shaders/src/shaders/test/fragment.glsl
+++ Shaders/src/shaders/test/fragment.glsl
@@ -1,6 +1,7 @@
 precision mediump float;
+varying float vRabdom;
 
 void main()
 {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = vec4(0.5, vRabdom, 1.0, 1.0);
 }
--- Shaders/src/shaders/test/vertex.glsl
+++ Shaders/src/shaders/test/vertex.glsl
@@ -3,8 +3,19 @@ uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
 attribute vec3 position;
+attribute float aRandom;
+
+varying float vRabdom;
 
 void main()
 {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(position,1.0);
+//    modelPosition.z += sin(modelPosition.x * 10.0) *0.1;
+    modelPosition.z += aRandom *0.1;
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPostion = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPostion;
+
+    vRabdom = aRandom;
 }
