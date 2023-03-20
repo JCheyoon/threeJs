4eba686a6aceb13393b146f754b32353c4abdecb
Cheyoon Jung <jessietwo94@gmail.com>
2023-03-20 13:01:42 +0100

    learning deri

--- three-react/src/Expreience.jsx
+++ three-react/src/Expreience.jsx
@@ -4,9 +4,15 @@ import {
   PivotControls,
   TransformControls,
   OrbitControls,
+  Html,
+  Text,
+  Float,
+  MeshReflectorMaterial,
 } from "@react-three/drei";
 const Experience = () => {
   const cubeRef = useRef();
+  const sphereRef = useRef();
+
   useFrame((state, delta) => {
     // cubeRef.current.rotation.y += delta;
     // groupRef.current.rotation.y += delta;
@@ -20,9 +26,18 @@ const Experience = () => {
       <ambientLight intensity={0.3} />
 
       <PivotControls anchor={[0, 0, 0]} depthTest={false}>
-        <mesh position-x={-2} scale={1}>
+        <mesh ref={sphereRef} position-x={-2} scale={1}>
           <sphereGeometry />
           <meshStandardMaterial color="orange" />
+          <Html
+            position={[1, 1, 0]}
+            wrapperClass="label"
+            center
+            distanceFactor={6}
+            occlude={[sphereRef, cubeRef]}
+          >
+            The Orange Sphere🍊
+          </Html>
         </mesh>
       </PivotControls>
 
@@ -34,8 +49,27 @@ const Experience = () => {
 
       <mesh rotation-x={Math.PI * -0.5} position={[0, -2, 0]} scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color="lightgreen" />
+        <MeshReflectorMaterial
+          mirror={0.75}
+          color="lightGreen"
+          resolution={512}
+          mixBlur={1}
+          blur={[1000, 1000]}
+        />
       </mesh>
+
+      <Float speed={5} floatIntensity={3}>
+        <Text
+          font="./bangers-v20-latin-regular.woff"
+          position-z={2}
+          color="salmon"
+          fontSize={1}
+          textAlign="center"
+          maxWidth={2}
+        >
+          I love text
+        </Text>
+      </Float>
     </>
   );
 };
