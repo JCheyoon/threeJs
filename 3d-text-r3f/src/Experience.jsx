1d56f692e4c0121b42a15763ab0b07bc267fec97
Cheyoon Jung <jessietwo94@gmail.com>
2023-03-28 13:28:21 +0200

    hello world

--- 3d-text-r3f/src/Experience.jsx
+++ 3d-text-r3f/src/Experience.jsx
@@ -1,18 +1,35 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import {
+  Center,
+  Text3D,
+  OrbitControls,
+  useMatcapTexture,
+} from "@react-three/drei";
+import { Perf } from "r3f-perf";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  const [matcapTexture] = useMatcapTexture("B6B8B1_994A24_315C81_927963", 256);
+  console.log(matcapTexture);
 
-        <Perf position="top-left" />
+  return (
+    <>
+      <Perf position="top-left" />
 
-        <OrbitControls makeDefault />
-
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
+      <OrbitControls makeDefault />
+      <Center>
+        <Text3D
+          font="./fonts/helvetiker_regular.typeface.json"
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+        >
+          Hello world <meshMatcapMaterial matcap={matcapTexture} />
+        </Text3D>
+      </Center>
     </>
+  );
 }
