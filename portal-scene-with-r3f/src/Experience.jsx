efa11d63fbd34e106bc5190d5fbf3ef1f0dec71b
Cheyoon Jung <jessietwo94@gmail.com>
2023-04-03 11:02:11 +0200

    end portal scene

--- portal-scene-with-r3f/src/Experience.jsx
+++ portal-scene-with-r3f/src/Experience.jsx
@@ -1,8 +1,35 @@
-import { Center, useGLTF, OrbitControls, useTexture } from "@react-three/drei";
+import {
+  shaderMaterial,
+  Sparkles,
+  Center,
+  useGLTF,
+  OrbitControls,
+  useTexture,
+} from "@react-three/drei";
+import * as THREE from "three";
+import { extend, useFrame } from "@react-three/fiber";
+import portalVertexShader from "./shaders/portal/vertex.glsl";
+import portalFragmentShader from "./shaders/portal/fragment.glsl";
+import { useRef } from "react";
+
+const PortalMaterial = shaderMaterial(
+  {
+    uTime: 0,
+    uColorStart: new THREE.Color("#ffffff"),
+    uColorEnd: new THREE.Color("#000000"),
+  },
+  portalVertexShader,
+  portalFragmentShader
+);
+extend({ PortalMaterial }); //jsx에서 <portalMaterial/> 이런식으로 쓸수있게 바꿔주는것
 
 const Experience = () => {
   const { nodes } = useGLTF("./model/portal.glb");
   const backedTexture = useTexture("/model/baked.jpg");
+  const portalMaterial = useRef();
+  useFrame((state, delta) => {
+    portalMaterial.current.uTime += delta;
+  });
 
   return (
     <>
@@ -22,11 +49,23 @@ const Experience = () => {
         <mesh
           geometry={nodes.poleLightB.geometry}
           position={nodes.poleLightB.position}
-        ></mesh>
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
         <mesh
           geometry={nodes.portalLight.geometry}
           position={nodes.portalLight.position}
-        ></mesh>
+          rotation={nodes.portalLight.rotation}
+        >
+          <portalMaterial ref={portalMaterial} />
+        </mesh>
+        <Sparkles
+          size={6}
+          scale={[4, 2, 4]}
+          position-y={1}
+          speed={0.2}
+          count={40}
+        />
       </Center>
     </>
   );
