f31480df3fd61af00d40e4d100c66fab72add798
Cheyoon Jung <jessietwo94@gmail.com>
2023-04-14 15:18:31 +0200

    learning

--- post-processing-with-r3f/src/Experience.jsx
+++ post-processing-with-r3f/src/Experience.jsx
@@ -1,17 +1,73 @@
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { Vignette, EffectComposer } from "@react-three/postprocessing";
-import { BlendFunction } from "postprocessing";
+import {
+  Bloom,
+  Glitch,
+  SSR,
+  EffectComposer,
+  DepthOfField,
+  Noise,
+} from "@react-three/postprocessing";
+import { useControls } from "leva";
+import { BlendFunction, GlitchMode } from "postprocessing";
+import Drunk from "./Drunk.jsx";
+import { useRef } from "react";
 
 export default function Experience() {
-  console.log(BlendFunction);
+  // const ssrProps = useControls("SSR Effect", {
+  //   temporalResolve: true,
+  //   STRETCH_MISSED_RAYS: true,
+  //   USE_MRT: true,
+  //   USE_NORMALMAP: true,
+  //   USE_ROUGHNESSMAP: true,
+  //   ENABLE_JITTERING: true,
+  //   ENABLE_BLUR: true,
+  //   temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+  //   temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+  //   maxSamples: { value: 0, min: 0, max: 1 },
+  //   resolutionScale: { value: 1, min: 0, max: 1 },
+  //   blurMix: { value: 0.5, min: 0, max: 1 },
+  //   blurKernelSize: { value: 8, min: 0, max: 8 },
+  //   blurSharpness: { value: 0.5, min: 0, max: 1 },
+  //   rayStep: { value: 0.3, min: 0, max: 1 },
+  //   intensity: { value: 1, min: 0, max: 5 },
+  //   maxRoughness: { value: 0.1, min: 0, max: 1 },
+  //   jitter: { value: 0.7, min: 0, max: 5 },
+  //   jitterSpread: { value: 0.45, min: 0, max: 1 },
+  //   jitterRough: { value: 0.1, min: 0, max: 1 },
+  //   roughnessFadeOut: { value: 1, min: 0, max: 1 },
+  //   rayFadeOut: { value: 0, min: 0, max: 1 },
+  //   MAX_STEPS: { value: 20, min: 0, max: 20 },
+  //   NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+  //   maxDepthDifference: { value: 3, min: 0, max: 10 },
+  //   maxDepth: { value: 1, min: 0, max: 1 },
+  //   thickness: { value: 10, min: 0, max: 10 },
+  //   ior: { value: 1.45, min: 0, max: 2 },
+  // });
 
+  const drunkRef = useRef();
   return (
     <>
       <color args={["#ffffff"]} attach="background" />
       <EffectComposer>
-        <Vignette offset={0.3} darkness={0.9} />
+        {/*<Glitch*/}
+        {/*  delay={[0.5, 1]}*/}
+        {/*  duration={[0.1, 0.3]}*/}
+        {/*  strength={[0.2, 0.4]}*/}
+        {/*  mode={GlitchMode.CONSTANT_MILD}*/}
+        {/*/>*/}
+        {/*<Noise blendFunction={BlendFunction.SOFT_LIGHT} />*/}
+        {/*<Bloom mipmapBlur intensitiy={0.5} luminanceThreshold={0} />*/}
+        {/*<DepthOfField*/}
+        {/*  focusDistance={0} // where to focus*/}
+        {/*  focalLength={0.02} // focal length*/}
+        {/*  bokehScale={3} // bokeh size*/}
+        {/*/>*/}
+        {/*<SSR />*/}
+
+        <Drunk ref={drunkRef} frequency={2} amplitude={0.2} />
       </EffectComposer>
+
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
@@ -26,7 +82,7 @@ export default function Experience() {
 
       <mesh castShadow position-x={2} scale={1.5}>
         <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
+        <meshStandardMaterial color="Purple" />
       </mesh>
 
       <mesh
@@ -36,7 +92,7 @@ export default function Experience() {
         scale={10}
       >
         <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+        <meshStandardMaterial color="greenYellow" metalness={0} roughness={0} />
       </mesh>
     </>
   );
