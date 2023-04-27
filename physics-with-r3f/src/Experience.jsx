d884743f527bbb2cf701cbfa92734c8ace21d689
Cheyoon Jung <jessietwo94@gmail.com>
2023-04-27 10:48:37 +0200

    learning physics

--- physics-with-r3f/src/Experience.jsx
+++ physics-with-r3f/src/Experience.jsx
@@ -1,8 +1,13 @@
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { RigidBody, Physics, Debug } from "@react-three/rapier";
+import { RigidBody, Physics, Debug, CuboidCollider } from "@react-three/rapier";
+import { useRef } from "react";
 
 export default function Experience() {
+  const cubeRef = useRef();
+  const cubeJump = () => {
+    cubeRef.current.applyImpulse({ x: 0, y: 5, z: 0 });
+  };
   return (
     <>
       <Perf position="top-left" />
@@ -15,29 +20,31 @@ export default function Experience() {
       <Physics>
         <Debug />
         <RigidBody colliders="ball">
-          <mesh castShadow position={[-2, 2, 0]}>
+          <mesh castShadow position={[0, 4, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
           </mesh>
         </RigidBody>
 
-        <RigidBody>
-          <mesh castShadow position={[2, 2, 0]}>
+        <RigidBody ref={cubeRef} position={[3, 1, 0]}>
+          <mesh castShadow onClick={cubeJump}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
           </mesh>
         </RigidBody>
 
-        <RigidBody colliders="hull">
-          <mesh
-            castShadow
-            position={[0, 1, 0]}
-            rotation={[Math.PI * 0.5, 0, 0]}
-          >
-            <torusGeometry args={[1, 0.5, 16, 32]} />
-            <meshStandardMaterial color="salmon" />
-          </mesh>
-        </RigidBody>
+        {/*<RigidBody*/}
+        {/*  colliders={false}*/}
+        {/*  position={[0, 1, 0]}*/}
+        {/*  rotation={[Math.PI * 0.5, 0, 0]}*/}
+        {/*>*/}
+        {/*  <CuboidCollider args={[1.5, 1.5, 0.5]}>*/}
+        {/*    <mesh castShadow>*/}
+        {/*      <torusGeometry args={[1, 0.5, 16, 32]} />*/}
+        {/*      <meshStandardMaterial color="salmon" />*/}
+        {/*    </mesh>*/}
+        {/*  </CuboidCollider>*/}
+        {/*</RigidBody>*/}
 
         <RigidBody type="fixed">
           <mesh receiveShadow position-y={-1.25}>
